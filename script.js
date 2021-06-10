a1b2798e088854ad90712a9775980031d47bba1a
Dmitriy <jatematik@gmail.com>
2021-06-10 23:43:39 +0600

    test commit

--- script.js
+++ script.js
@@ -2,12 +2,8 @@
 
 const employers = ['АртеМ', 'максим', 'Владимир', 'сергей', 'НикиТа', 'евГений', ' Дарья', ' ', 'виктория ', 'ЕкаТерина', '', ' Андрей ', 'КИРИЛЛ'];
 const nameCourse = 'Базовый React';
-const command = [];
-for (let i = 0; i < employers.length; i++) {
-	if (employers[i].length > 0 && employers[i].trim() != '') {
-		command.push(employers[i]);
-	}
-}
+let command = employers.filter(item => item.length > 0 && item.trim() !== '');;
+
 for (let i = 0; i < command.length; i++) {
 	command[i] = command[i].toLowerCase().trim();
 	command[i] = command[i][0].toUpperCase() + command[i].slice(1);
@@ -19,27 +15,18 @@ const data = {
 	add: ['styled-components', 'firebase']
 };
 
-const calcCash = (...own) => {
-	own = own || 0;
-	const everyCash = own;
-	let total = own[0];
-	for (let i = 0; i < everyCash[1].length; i++) {
-		total += +everyCash[1][i];
-	}
-	return total;
-}
+const calcCash = (own = 0) => typeof own === 'object' ? own.reduce((accum, item) => accum + item) : own;
 
-const lesson = calcCash(null, data.cash);
+const lesson = calcCash(data.cash);
 
-const makeBusiness = (director, teacher = 'Максим', allModule, gang, course) => {
+const makeBusiness = ([director, teacher = 'Максим', allModule, gang, course]) => {
 	const [jsx] = data.react;
 	const sumTech = data.react.concat(data.add, 'и другие');
-	console.log(`Стартуем новый курс: ${course}. Владелец: ${director}, преподаватель: ${teacher}. Всего уроков: ${allModule}. 
-Команда Академии: ${gang}`);
-	console.log(`Первое что изучим будет ${jsx}. Он очень похож на HTML!`);
-	console.log('Технологии которые мы изучим: ');
-	console.log.apply(this, sumTech);
-
+	console.log(`Стартуем новый курс: "${course}". Владелец: ${director}, преподаватель: ${teacher}. Всего уроков: ${allModule}. 
+Команда Академии: ${gang}
+Первое что изучим будет ${jsx}. Он очень похож на HTML!
+Технологии которые мы изучим: 
+${sumTech}`);
 }
 
-makeBusiness.apply(this, ['Артем', , lesson, command, nameCourse]);
+makeBusiness(['Артем', , lesson, command, nameCourse]);
